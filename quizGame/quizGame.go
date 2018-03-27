fb383e0bf224912e61ddea32c90a07e6b204b7f0
flevinkelming <flevinkelming@anonymous.local>
2018-03-26 21:16:01 -0400

    Adding source to github.

---
+++ quizGame/quizGame.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+type problem struct {
+	Q, A string
+}
+
+type problems []problem
+
+func main() {
+	csvFileName := flag.String("csv", "problems.csv", "A .csv file in the format of"+
+		"'question,answer'")
+	timeLimit := flag.Int("limit", 30, "The time allotted for the quiz in seconds.")
+	flag.Parse()
+
+	f, err := os.Open(*csvFileName)
+	if err != nil {
+		exitOnFail(fmt.Sprintf("Failed to load .csv file: %s\n", *csvFileName))
+	}
+
+	reader := csv.NewReader(f)
+	rows, err := reader.ReadAll()
+	if err != nil {
+		exitOnFail(fmt.Sprintf("An error occurred while parsing the csv: %s\n", err.Error()))
+	}
+
+	quiz := parseCSVData(rows)
+
+	correct := 0
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
+	for i, p := range quiz {
+		fmt.Printf("Question #%d: %s? ", i+1, p.Q)
+
+		answerChan := make(chan string)
+		go func() {
+			var answer string
+			fmt.Scanf("%s", &answer)
+			answerChan <- answer
+		}()
+
+		select {
+		case <-timer.C:
+			fmt.Printf("\nYou answered %d/%d questions correctly.", correct, len(quiz))
+			return
+		case answer := <-answerChan:
+			if answer == p.A {
+				correct++
+			}
+		}
+	}
+
+	fmt.Printf("You answered %d/%d questions correctly.", correct, len(quiz))
+}
+
+func exitOnFail(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
+}
+
+func parseCSVData(rows [][]string) problems {
+	quiz := make(problems, len(rows))
+	for i, row := range rows {
+		quiz[i] = problem{
+			Q: strings.TrimSpace(row[0]),
+			A: strings.TrimSpace(row[1]),
+		}
+	}
+
+	return quiz
+}
